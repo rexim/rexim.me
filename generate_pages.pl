cec87cfb422651903eadec3e81e071af1bd1b00c
rexim <reximkut@gmail.com>
2015-07-08 00:19:09 +0600

    Move pagination to a separate module (#9)

--- generate_pages.pl
+++ generate_pages.pl
@@ -7,33 +7,10 @@ use warnings;
 use utf8;
 
 use File::Copy::Recursive qw(dircopy);
-use Data::Dumper;
 use Template;
-use List::Util qw[min max];
-use POSIX qw/ceil/;
 
 use Olyvova::Post qw(compile_posts_dir);
-
-sub filter_posts_by_current_page($$$) {
-    my ($posts, $page_size, $current_page) = @_;
-
-    my $posts_count = @$posts;
-    my $lower_bound = $current_page * $page_size;
-    my $upper_bound = min(($current_page + 1) * $page_size, $posts_count) - 1;
-
-    if ($lower_bound <= $upper_bound) {
-        my @xs = @$posts[$lower_bound .. $upper_bound];
-        return \@xs;
-    } else {
-        return [];
-    }
-}
-
-sub get_pages_count($$) {
-    my ($posts, $page_size) = @_;
-    my $posts_count = @$posts;
-    return ceil($posts_count / $page_size);
-}
+use Olyvova::Pagination qw(filter_posts_by_current_page get_pages_count make_paginator);
 
 sub make_index_page_name($) {
     my ($page_number) = @_;
@@ -42,28 +19,6 @@ sub make_index_page_name($) {
     } else {
         return "index.html";
     }
-}
-
-sub make_paginator($$) {
-    my ($pages_count, $current_page) = @_;
-    my $paginator = {
-        page_buttons => []
-    };
-
-    for (my $i = 0; $i < $pages_count; $i++) {
-        my $page_button = {
-            label => "$i",
-            is_current => $i == $current_page
-        };
-
-        if (!$page_button->{is_current}) {
-            $page_button->{href} = make_index_page_name($i);
-        }
-
-        push $paginator->{page_buttons}, $page_button
-    }
-
-    return $paginator;
 }
 
 sub main {
@@ -110,7 +65,8 @@ sub main {
     for (my $i = 0; $i < $pages_count; $i++) {
         $generate_file->(make_index_page_name($i), "index.tt",
                          { posts => filter_posts_by_current_page($posts, $page_size, $i),
-                           paginator => make_paginator($pages_count, $i),
+                           paginator => make_paginator($pages_count, $i,
+                                                       \&make_index_page_name),
                            basurl => "http://rexim.me/" });
     }
 }
